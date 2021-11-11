6f8e766d6dcaa0ee6166aab4fcd68b7b4271be43
arias95martin <arias95martin@gmail.com>
2021-11-11 18:27:57 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild,AfterViewInit } from '@angular/core';
+import { Coin } from './coin';
+import { ApiService } from './api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort, Sort} from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-criptos';
+export class AppComponent implements OnInit,AfterViewInit{
+
+	coins: Coin[] = []
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	/* Columnas de la tabla */
+	public displayedColumns = ['image','name', 'symbol', 'current_price', 'price_change_percentage_24h','total_volume'];
+
+	/* Datos de la tabla */
+	public dataSource = new MatTableDataSource<Coin>();
+
+	constructor(private coinApiService: ApiService){
+	}
+	ngAfterViewInit() {
+		this.dataSource.sort = this.sort
+		this.dataSource.paginator = this.paginator;
+	}
+	ngOnInit() {
+		//call this method on component load
+		this.getCoinsInformation();
+	}
+	getCoinsInformation() {
+		this.coinApiService.getCoinsInformation()
+			.subscribe((res) => {
+				console.log(res);
+				this.dataSource.data = res;
+			})
+	}
+	announceSortChange() {
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+
+/* Importaciones de Angular Material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from "./app.material.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
